1307112827b4d051d6005fbbd63e0ee942632c4d
garricksu <garricksuuu@gmail.com>
2020-12-03 17:45:13 -0800

    Set up server, started registering route

---
+++ server.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const db = require('./db')
+const cors = require('cors')
+
+const PORT = process.env.PORT || 5000
+
+// middleware
+
+app.use(express.json())
+app.use(cors())
+
+app.use('/api/auth', require('./routes/jwtAuth'))
+
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`)
+})
